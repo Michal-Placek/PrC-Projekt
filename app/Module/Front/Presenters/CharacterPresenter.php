bace24df5ec7a7c55228ef5d8724d9d1ea6b4494
Michal-Placek <michal.placek@student.ossp.cz>
2022-11-07 12:21:45 +0100

    Yes
    Co-authored-by: KarelNakladal <KarelNakladal@users.noreply.github.com>

--- app/Module/Front/Presenters/CharacterPresenter.php
+++ app/Module/Front/Presenters/CharacterPresenter.php
@@ -38,6 +38,17 @@ final class CharacterPresenter extends Nette\Application\UI\Presenter
 		return $form;
 	}
 
+	public function renderAvatar(){
+		$this->template->race = 0;
+
+		$this->template->bodyId = 0;
+		$this->template->headId = 0;
+		$this->template->eyesId = 0;
+		$this->template->hairId = 0;
+		$this->template->mouthId = 0;
+		$this->template->noseId = 0;
+	}
+
 	public function onNewCharacterCreated(Form $form, \stdClass $data)
 	{
 		$this->characterFacade->addChar($data->name, $this->getUser()->getId());
@@ -46,7 +57,65 @@ final class CharacterPresenter extends Nette\Application\UI\Presenter
 		$this->redirect('Character:avatar');
 	}
 
-	public function actionEditCharacter($part, $action){
+	public function handleeditCharacter($part, $action){
 		
+		switch($part){
+
+		case 'race':
+			if($action == 1){
+				$this->template->race++;
+			}else{
+				$this->template->race--;
+			}
+			break;
+		case 'body':
+			if($action == 1){
+				$this->template->bodyId++;
+			}else{
+				$this->template->bodyId--;
+			}
+			break;
+		case 'head':
+			if($action == 1){
+				$this->template->headId++;
+			}else{
+				$this->template->headId--;
+			}
+
+			break;
+		case 'eyes':
+			if($action == 1){
+				$this->template->eyesId++;
+			}else{
+				$this->template->eyesId--;
+			}
+
+			break;
+		case 'hair':
+			if($action == 1){
+				$this->template->hairId++;
+			}else{
+				$this->template->hairId--;
+			}
+
+			break;
+		case 'mouth':
+			if($action == 1){
+				$this->template->mouthId++;
+			}else{
+				$this->template->mouthId--;
+			}
+
+			break;
+		case 'nose':
+			if($action == 1){
+				$this->template->noseId = $this->template->noseId + 1;
+			}else{
+				$this->template->noseId = $this->template->noseId - 1;
+			}
+			break;
+	
+		}
+		$this->redrawControl('characterCreator');
 	}
 }
